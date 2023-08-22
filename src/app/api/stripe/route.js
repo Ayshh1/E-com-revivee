3e1613ea26518384cfd69c49b88c4e5b66587b21
Ayshh1 <as1526314@gmail.com>
2023-08-22 23:54:23 +0500

    firt

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -17,8 +17,8 @@ export async function POST(req) {
         payment_method_types: ["card"],
         line_items: res,
         mode: "payment",
-        success_url: "https://fullstack-e-commerce.vercel.app/checkout" + "?status=success",
-        cancel_url: "https://fullstack-e-commerce.vercel.app/checkout" + "?status=cancel",
+        success_url: "https://e-commerce-xjyr.vercel.app/checkout" + "?status=success",
+        cancel_url: "https://e-commerce-xjyr.vercel.app/checkout" + "?status=cancel",
       });
 
       return NextResponse.json({
