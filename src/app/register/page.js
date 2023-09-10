ff09672a226b2962c8aef56a84c290b56f53fc51
Ayesha Shahzad <123255323+Ayshh1@users.noreply.github.com>
2023-09-10 16:11:40 +0500

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -72,8 +72,8 @@ export default function Register() {
       <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto xl:px-5 lg:flex-row">
         <div className="flex flex-col justify-center items-center w-full lg:pr-10 lg:pl-10 sm:pr-7 sm:pl-7 lg:flex-row">
           <div className="w-full mt-10 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12">
-            <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl relative z-10">
-              <p className="w-full text-4xl font-medium text-center font-serif">
+            <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl relative z-10 sm:object-cover sm:visible text-black">
+              <p className="w-full text-4xl font-medium text-center font-serif sm:object-cover sm:visible text-black">
                 {isRegistered
                   ? "Registration Successfull !"
                   : "Sign up for an account"}
