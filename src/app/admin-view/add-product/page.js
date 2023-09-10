7d69088f68dbcb215599b543eba1d43c7208e0f2
Ayesha Shahzad <123255323+Ayshh1@users.noreply.github.com>
2023-09-10 17:02:32 +0500

    Update page.js

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -149,8 +149,8 @@ export default function AdminAddNewProduct() {
 
   return (
     <div className="w-full mt-5 mr-0 mb-0 ml-0 relative ">
-      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
-        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8">
+      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative sm:visible sm:object-cover text-black">
+        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8 sm:visible sm:object-cover text-black">
           <input
             accept="image/*"
             max="1000000"
@@ -158,7 +158,7 @@ export default function AdminAddNewProduct() {
             onChange={handleImage}
           />
 
-          <div className="flex gap-2 flex-col">
+          <div className="flex gap-2 flex-col sm:visible sm:object-cover text-black">
             <label>Available sizes</label>
             <TileComponent
               selected={formData.sizes}
