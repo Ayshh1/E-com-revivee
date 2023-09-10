caa045edcd8b819f72bb35d5a83932211e824d96
Ayesha Shahzad <123255323+Ayshh1@users.noreply.github.com>
2023-09-10 16:04:07 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
   <div className="max-w-screen-xl mx-auto lg:grid lg:grid-cols-12 gap-8">
     
     <div className="lg:col-span-7">
-      <h1 className="pt-5 text-4xl font-extrabold tracking-tight leading-none md:text-4xl lg:text-5xl xl:text-6xl object-cover visible sm:block">
+      <h1 className="pt-5 sm:text-2xl text-3xl text-black font-extrabold tracking-tight leading-none md:text-4xl lg:text-5xl xl:text-6xl sm:object-cover sm:visible sm:block">
         Top-Tier Style Curation
       </h1>
       <p className="mt-4 text-gray-500 text-lg lg:text-xl">
