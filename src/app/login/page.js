74d3b8e091d60e7a986c2e6593c7c86487c0e8b4
Ayesha Shahzad <123255323+Ayshh1@users.noreply.github.com>
2023-09-10 16:15:25 +0500

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -78,7 +78,7 @@ export default function Login() {
         <div className="flex flex-col justify-center items-center w-full lg:pr-10 lg:pl-10 sm:pr-7 sm:pl-7 lg:flex-row">
           <div className="w-full mt-10 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12">
             <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl relative z-10">
-              <p className="w-full text-4xl font-medium text-center font-serif">
+              <p className="w-full text-4xl font-medium text-center font-serif sm:object-cover sm:visible text-black">
                 Login
               </p>
               <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8 sm:object-cover sm:visible text-black">
