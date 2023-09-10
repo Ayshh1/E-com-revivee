e1f8eff0d30e6b683aec31d772bbaf480f239c0e
Ayesha Shahzad <123255323+Ayshh1@users.noreply.github.com>
2023-09-10 16:24:14 +0500

    Update index.js

--- src/components/CommonListing/index.js
+++ src/components/CommonListing/index.js
@@ -15,12 +15,12 @@ export default function CommonListing({ data }) {
 
   return (
     <section className="bg-white py-12 sm:py-16 hover:shadow-xl-600">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 hover:shadow-xl">
-        <div className="mt-10 grid grid-cols-2 gap-6 sm:grid-cols-4 sm:gap-4 lg:mt-16">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 hover:shadow-xl sm:object-cover sm:visible text-black">
+        <div className="mt-10 grid grid-cols-2 gap-6 sm:grid-cols-4 sm:gap-4 lg:mt-16 sm:object-cover sm:visible text-black">
           {data && data.length
             ? data.map((item) => (
                 <article
-                  className="relative flex flex-col overflow-hidden border cursor-pointer hover:shadow-xl"
+                  className="relative flex flex-col overflow-hidden border cursor-pointer hover:shadow-xl sm:object-cover sm:visible text-black"
                   key={item._id}
                 >
                   <ProductTile item={item} />
